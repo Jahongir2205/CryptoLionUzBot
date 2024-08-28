1c35803ab656436b964355d59799911ef05e5702
khakimovSamdu <allamurodxakimov@gmail.com>
2024-08-28 22:31:57 +0500

    keyboards

--- keyboards.py
+++ keyboards.py
@@ -5,6 +5,7 @@ replay_guruh = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="CryptoLionUz", url="https://t.me/CryptoLionUz")],
     [InlineKeyboardButton(text="UzCryptoKing", url="https://t.me/UzCryptoKing")],
     [InlineKeyboardButton(text="Dasturchining Kundaligi", url="https://t.me/DasturchiningKundaligi")],
+    [InlineKeyboardButton("Tasdiqlash", callback_data='verify_subscriptions')]
     ])
 
 replay_tasqid = ReplyKeyboardMarkup(
@@ -13,8 +14,6 @@ replay_tasqid = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True
 )
-
-kalit_keyboard = [
-            [InlineKeyboardButton("Kalitni olish", url="http://t.me/CryptoLionUzBot/HamsterKeys")]
-        ]
-        
+kalit_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton("Kalitni olish", url="http://t.me/CryptoLionUzBot/HamsterKeys")]
+    ])
